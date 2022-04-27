a9e1e5e1e4e01808623fc4e04f4a349fba44c5c9
matheusmski1 <matheusmski1@gmail.com>
2022-04-27 18:39:16 -0300

    Initial commit

---
+++ pages/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+# pages/views.py
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+
+# Create your views here.
